bcda0cced8f0c0c8fdccc5c1b09a8b477436b1d2
Jonah Schreiner <jonahmschreiner@gmail.com>
2022-05-17 21:27:51 -0500

    Rating Obj and almost Hypothesis Obj

---
+++ UnboundAI/src/unboundStruct/Hypothesis.java
@@ -0,0 +1,47 @@
+package unboundStruct;
+import java.util.*;
+import java.io.*;
+public class Hypothesis {
+	public File actionFile;
+	public String actionName;
+	public List<Rating> ratings;
+	public Hypothesis() {
+		this.actionName = "action not set";
+		this.ratings = new ArrayList<Rating>();
+	}
+	public Hypothesis(String actionFilePath, String actionNameIn, List<Rating> ratingsIn) {
+		this.actionFile = new File(actionFilePath);
+		this.actionName = actionNameIn;
+		this.ratings = ratingsIn;
+	}
+	public Hypothesis(String actionFilePath, String actionNameIn) {
+		this.actionFile = new File(actionFilePath);
+		this.actionName = actionNameIn;
+	}
+	public void initRatings() {
+		//get all existing goal files and make a rating of 0 for each one
+		File goalDir = new File("/home/agi/Desktop/eclipse/UnboundAI/BeliefStorage/Goals");
+		File errorGoalDir = new File("/home/agi/Desktop/eclipse/UnboundAI/BeliefStorage/ErrorGoal");
+		String[] goalPaths = goalDir.list();
+		String[] errorGoalPath = errorGoalDir.list();
+		List<String> paths = new ArrayList<String>();
+		for (int i = 0; i < goalPaths.length; i++) {
+			paths.add("/home/agi/Desktop/eclipse/UnboundAI/BeliefStorage/Goals/" + goalPaths[i]);
+		}
+		for (int i = 0; i < errorGoalPath.length; i++) {
+			paths.add("/home/agi/Desktop/eclipse/UnboundAI/BeliefStorage/ErrorGoal/" + errorGoalPath[i]);
+		}
+		List<Rating> newRatings = new ArrayList<Rating>();
+		for (int i = 0; i < paths.size(); i++) {
+			File goal = new File(paths.get(i));
+			String goalName = goal.getName();
+			int ratingValue = 0;
+			Rating rating = new Rating(paths.get(i), goalName, ratingValue);
+			newRatings.add(rating);
+		}
+		this.ratings = newRatings;
+	}
+	public void updateRatings(List<Rating> ratingsIn) {
+		//use the ratings in to update the appropriate ratings
+	}
+}
---
+++ UnboundAI/src/unboundStruct/Rating.java
@@ -0,0 +1,38 @@
+package unboundStruct;
+import java.io.File;
+public class Rating {
+	public File goalFile;
+	public String goalName;
+	public int rating;	
+	public Rating() {
+		this.goalName = "goal not set";
+		this.rating = 0;
+	}
+	public Rating(String goalFilePath, String goalNameIn, int ratingIn) {
+		this.goalFile = new File(goalFilePath);
+		this.goalName = goalNameIn;
+		this.rating = ratingIn;
+	}
+	@Override
+	public String toString(){
+		return "Name: " + this.goalName + "\nRating: " + this.rating;
+	}
+	@Override
+	public boolean equals(Object o) {
+		if (o == this){
+			return true;
+		}
+		if (o instanceof Rating){
+			Rating c = (Rating) o;
+			boolean output = false;
+			if ((c.goalName.equals(this.goalName)) && (c.rating == this.rating)){
+				output = true;
+			} else {
+				output = false;
+			}
+			return output;
+		} else {
+			return false;
+		}	
+	}
+}
