544c217015ce2f27d9bc21b5a05ebe9367f7da8d
Jonah Schreiner <jonahmschreiner@gmail.com>
2022-12-08 14:49:57 -0600

    commented out testing code/junk after previous commit

--- WhiteSpikeAI/MainLLF/LLF.java
+++ WhiteSpikeAI/MainLLF/LLF.java
@@ -65,14 +65,14 @@ public class LLF {
 		List<Integer> actionQueue = new ArrayList<Integer>();
 		
 		//for testing
-		int testCount = 0;
-		boolean testFlag = false;
+//		int testCount = 0;
+//		boolean testFlag = false;
 		//
 		try {
 			boolean continueLooping = true;
 		while (continueLooping) {
 			//for testing
-			testCount++;
+//			testCount++;
 //			if (testCount == 5) {
 //				Point testPoint = MouseInfo.getPointerInfo().getLocation();
 //				System.out.println("");
@@ -84,27 +84,27 @@ public class LLF {
 //					
 //				}
 //			}
-			if (activitiesToSolveQueue.size() > 0) {
-				System.out.println("some issue: " + activitiesToSolveQueue.get(0));
-			}
-			
-			if (activitiesToSolveQueue.size() > 0) {
-				if (activitiesToSolveQueue.get(0) == 30 || activitiesToSolveQueue.get(0) == 31) {
-					Point testPoint = MouseInfo.getPointerInfo().getLocation();
-					System.out.println("I changed the env");
-					testFlag = true;
-					try {
-						Thread.sleep(4000);
-						Util util = new Util();
-						util.moveMouseToCoordinates(testPoint.x, testPoint.y);
-					} catch (Exception e) {
-				
-					}
-				}
-			}
-			if (testCount == 301) {
-				System.out.println();
-			}
+//			if (activitiesToSolveQueue.size() > 0) {
+//				System.out.println("some issue: " + activitiesToSolveQueue.get(0));
+//			}
+//			
+//			if (activitiesToSolveQueue.size() > 0) {
+//				if (activitiesToSolveQueue.get(0) == 30 || activitiesToSolveQueue.get(0) == 31) {
+//					Point testPoint = MouseInfo.getPointerInfo().getLocation();
+//					System.out.println("I changed the env");
+//					testFlag = true;
+//					try {
+//						Thread.sleep(4000);
+//						Util util = new Util();
+//						util.moveMouseToCoordinates(testPoint.x, testPoint.y);
+//					} catch (Exception e) {
+//				
+//					}
+//				}
+//			}
+//			if (testCount == 301) {
+//				System.out.println();
+//			}
 			//
 			
 			
@@ -113,16 +113,6 @@ public class LLF {
 			fw.append(" start of new life loop iteration\n");
 			fw.flush();
 			activitiesToSolveQueue = SetUpActivitiesToSolveQueueIfNecessary.setup(activitiesToSolveQueue, fw, myConnection);
-			//for testing TODO
-			System.out.println("wtf: " + testCount);
-			if (testCount == 1) {
-				activitiesToSolveQueue.set(0, 30);
-				activitiesToSolveQueue.set(1, 31);
-			}
-			System.out.println("fml: " + activitiesToSolveQueue.get(0));
-			System.out.println("test1: " + activitiesToSolveQueue.get(0).equals(31));
-			System.out.println("test2: " + (activitiesToSolveQueue.get(0) == 31));
-			//
 			System.out.println("activities to solve set up if necessary");
 			fw.append(" activities to solve queue set up (count: " + activitiesToSolveQueue.size() + ")\n");
 			fw.flush();
@@ -170,10 +160,10 @@ public class LLF {
 			int currentActivityToSolveID = activitiesToSolveQueue.get(0);
 			
 			//for testing
-			if (currentActivityToSolveID == 31 && testFlag) {
-				System.out.println("");
-			}
-			testFlag = false;
+//			if (currentActivityToSolveID == 31 && testFlag) {
+//				System.out.println("");
+//			}
+//			testFlag = false;
 			//
 			Sense s = GetSenseAssociatedWithActivity.execute(env, currentActivityToSolveID, myConnection);
 			System.out.println("sense associated with activity gotten");
